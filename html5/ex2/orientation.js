4ee0849e612eda1e7747470b8f364309c7729be2
minimalkid1 <sabau97@yahoo.com>
2018-10-29 12:31:52 +0200

    Update html5/ex2/orientation.js

--- html5/ex2/orientation.js
+++ html5/ex2/orientation.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML="Business version: 2018.10.29.0";
+document.getElementById("id_business_version").innerHTML="Business version: 2018.10.29.1";
 window.addEventListener("deviceorientation",on_device_orientation);
 
 function on_device_orientation(e)
