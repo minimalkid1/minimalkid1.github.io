db014d5d3a1b85c26ad848f2d3952ffc03803e74
minimalkid1 <sabau97@yahoo.com>
2018-10-29 12:23:27 +0200

    Orinetation

---
+++ html5/ex2/orientation.js
@@ -0,0 +1,9 @@
+document.getElementById("id_business_version").innerHTML="Business version: 2018.10.29.0";
+window.addEventListener("deviceorientation",on_device_orientation);
+
+function on_device_orientation(e)
+{
+	document.getElementById("id_alpha").innerHTML = e.alpha;
+	document.getElementById("id_beta").innerHTML = e.beta;
+	document.getElementById("id_gama").innerHTML = e.gama;
+}
