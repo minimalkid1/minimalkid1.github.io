9fbd2612241f60f077dc8b56d30a381777466b7c
minimalkid1 <sabau97@yahoo.com>
2018-11-05 13:41:44 +0200

    rotation

--- html5/ex2/orientation.js
+++ html5/ex2/orientation.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML="Business version: 2018.11.05.1";
+document.getElementById("id_business_version").innerHTML="Business version: 2018.11.05.2";
 window.addEventListener("deviceorientation",on_device_orientation);
 window.addEventListener("devicemotion",on_device_motion);
 
@@ -28,4 +28,7 @@ function on_device_motion(e)
 	document.getElementById("id_acc_z").innerHTML = Math.round(acc_z * 100) / 100;
 	document.getElementById("id_acc_x").innerHTML = Math.round(acc_x * 100) / 100;
 	document.getElementById("id_acc_y").innerHTML = Math.round(acc_y * 100) / 100;
+	
+	document.getElementById("id_rot_x").innerHTML = Math.round(Math.atan(acc_x / acc_z) * 100) / 100;
+	document.getElementById("id_rot_y").innerHTML = Math.round(Math.atan(acc_y / acc_z) * 100) / 100;
 }
