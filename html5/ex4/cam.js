09b2ece344d367733953621f16f2ea9509161e0f
minimalkid1 <sabau97@yahoo.com>
2018-11-12 11:35:32 +0200

    videoimage

--- html5/ex4/cam.js
+++ html5/ex4/cam.js
@@ -1,5 +1,7 @@
 document.getElementById("id_business_version").innerHTML="Business version: 2018.11.12.1";
 document.getElementById("id_button").addEventListener("click" , start);
+document.getElementById("id_video").addEventListener("touchstart",on_touch);
+document.getElementById("id_video").addEventListener("mousedown",on_touch);
 
 function on_success(e)
 {
@@ -16,4 +18,14 @@ function start()
 	var c = {audio: true , video: true};
 	
 	navigator.mediaDevices.getUserMedia(c).then(on_success).catch(on_failure);
+}
+
+function on_touch(e)
+{
+	var canvas = document.getElementById("id_canvas");
+	var context = camvas.getContext("2d");
+	var video = document.getElementById("id_video");
+	
+	context.drawImage(video , 0 , 0);
+	
 }
