6bab298d34e64d499f21a03e042821d21e85283b
minimalkid1 <sabau97@yahoo.com>
2018-11-12 11:39:56 +0200

    fix

--- html5/ex4/cam.js
+++ html5/ex4/cam.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML="Business version: 2018.11.12.2";
+document.getElementById("id_business_version").innerHTML="Business version: 2018.11.12.3";
 document.getElementById("id_button").addEventListener("click" , start);
 document.getElementById("id_video").addEventListener("touchstart",on_touch);
 document.getElementById("id_video").addEventListener("mousedown",on_touch);
@@ -23,7 +23,7 @@ function start()
 function on_touch(e)
 {
 	var canvas = document.getElementById("id_canvas");
-	var context = camvas.getContext("2d");
+	var context = canvas.getContext("2d");
 	var video = document.getElementById("id_video");
 	
 	context.drawImage(video , 0 , 0);
