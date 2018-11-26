cb10ef91fcb309c56982a49fc1a5c5106b47dcf7
minimalkid1 <sabau97@yahoo.com>
2018-11-26 11:12:53 +0200

    fix2

--- html5/ex5/touch.js
+++ html5/ex5/touch.js
@@ -9,7 +9,7 @@ function on_touch_start(e)
 	{
 	var context = canvas.getContext("2d");
 	context.beginPath();
-	context.arc(e.changedTouches.item[i].pageX , e.changedTouches.item[i].pageY , 10 , 0 , 2 * Math.PI);
+	context.arc(e.changedTouches.item(i).pageX , e.changedTouches.item(i).pageY , 10 , 0 , 2 * Math.PI);
 	context.stroke();
 	}
 }
