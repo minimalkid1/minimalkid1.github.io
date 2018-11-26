77f800f7e57afd166291044fc7e3f1843a41de1d
minimalkid1 <sabau97@yahoo.com>
2018-11-26 12:38:28 +0200

    multi touch

--- html5/ex5/touch.js
+++ html5/ex5/touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML="Business version: 2018.11.26.9";
+document.getElementById("id_business_version").innerHTML="Business version: 2018.11.26.10";
 
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
@@ -6,7 +6,9 @@ canvas.addEventListener("touchmove", on_touch_move);
 
 var canvas_bounding_rect = canvas.getBoundingClientRect();
 
-var last_pos = {x: 0, y: 0};
+var last_pos = {x: 0, y: 0 , id: 0};
+
+var positii = {};
 
 function on_touch_start(e)
 {
@@ -19,8 +21,8 @@ function on_touch_start(e)
 	context.arc(e.changedTouches.item(i).pageX -canvas_bounding_rect.left , 
 	e.changedTouches.item(i).pageY - canvas_bounding_rect.top , 10 , 0 , 2 * Math.PI);
 	context.stroke();
-	last_pos.x = e.changedTouches.item(i).pageX;
-	last_pos.y = e.changedTouches.item(i).pageY;
+	var last_pos = {x: e.changedTouches.item(i).pageX, y: e.changedTouches.item(i).pageY , id: e.changedTouches.item(i).identifier};
+	pozitii.push(last_pos);
 	}
 }
 
@@ -30,10 +32,15 @@ function on_touch_move(e)
 	
 	for(var i=0; i < e.changedTouches.length; i++)
 	{
+		var j =0;
+		for(; j < pozitii.length; j++)
+			if(pozitii(j).id == e.changedTouches.item(i).identifier)
+				break;
+			
 	var context = canvas.getContext("2d");
 	context.beginPath();
 	context.lineWidth = 20;
-	context.moveTo(last_pos.x -canvas_bounding_rect.left, last_pos.y -canvas_bounding_rect.top);
+	context.moveTo(pozitii(j).x -canvas_bounding_rect.left, pozitii(j).y -canvas_bounding_rect.top);
 	context.lineTo(e.changedTouches.item(i).pageX -canvas_bounding_rect.left , e.changedTouches.item(i).pageY - canvas_bounding_rect.top);
 	context.stroke();
 	context.beginPath();
@@ -42,7 +49,8 @@ function on_touch_move(e)
 	e.changedTouches.item(i).pageY - canvas_bounding_rect.top , 10 , 0 , 2 * Math.PI);
 	context.stroke();
 	
-	last_pos.x = e.changedTouches.item(i).pageX;
-	last_pos.y = e.changedTouches.item(i).pageY;
+	pozitii(j).x = e.changedTouches.item(i).pageX;
+	pozitii(j).y = e.changedTouches.item(i).pageY;
+	
 	}
 }
