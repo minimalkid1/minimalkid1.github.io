8da26ba061484e66f0f29c1f3734541c02a50581
minimalkid1 <sabau97@yahoo.com>
2018-11-26 11:22:44 +0200

    fixdrawoncanvas

--- html5/ex5/touch.js
+++ html5/ex5/touch.js
@@ -1,7 +1,9 @@
-document.getElementById("id_business_version").innerHTML="Business version: 2018.11.26.1";
+document.getElementById("id_business_version").innerHTML="Business version: 2018.11.26.2";
 
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
+
+var canvas_bounding_rect = canvas.getBoundingClientRect();
 
 function on_touch_start(e)
 {
@@ -9,7 +11,8 @@ function on_touch_start(e)
 	{
 	var context = canvas.getContext("2d");
 	context.beginPath();
-	context.arc(e.changedTouches.item(i).pageX , e.changedTouches.item(i).pageY , 10 , 0 , 2 * Math.PI);
+	context.arc(e.changedTouches.item(i).pageX -canvas_bounding_rect.left , 
+	e.changedTouches.item(i).pageY - canvas_bounding_rect.top , 10 , 0 , 2 * Math.PI);
 	context.stroke();
 	}
 }
