b0b77f1e3583bb28bfd841e1c589b59b3f9f68db
minimalkid1 <sabau97@yahoo.com>
2018-11-26 11:39:32 +0200

    drawline

--- html5/ex5/touch.js
+++ html5/ex5/touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML="Business version: 2018.11.26.4";
+document.getElementById("id_business_version").innerHTML="Business version: 2018.11.26.5";
 
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
@@ -6,24 +6,35 @@ canvas.addEventListener("touchmove", on_touch_move);
 
 var canvas_bounding_rect = canvas.getBoundingClientRect();
 
+var last_pos = (x: 0, y: 0);
+
 function on_touch_start(e)
 {
 	for(var i=0; i < e.changedTouches.length; i++)
 	{
+		e.preventDefault();
+		
 	var context = canvas.getContext("2d");
 	context.beginPath();
 	context.arc(e.changedTouches.item(i).pageX -canvas_bounding_rect.left , 
 	e.changedTouches.item(i).pageY - canvas_bounding_rect.top , 10 , 0 , 2 * Math.PI);
 	context.stroke();
+	
+	last_pos.x = e.changedTouches.item(i).pageX;
+	last_pos.y = e.changedTouches.item(i).pageY;
 	}
 }
 
 function on_touch_move(e)
 {
+	e.preventDefault();
+	
 	for(var i=0; i < e.changedTouches.length; i++)
 	{
 	var context = canvas.getContext("2d");
 	context.beginPath();
+	context.moveTo(last_pos.x -canvas_bounding_rect.left, last_pos.y -canvas_bounding_rect.top);
+	context.lineTo(e.changedTouches.item(i).pageX -canvas_bounding_rect.left , e.changedTouches.item(i).pageY - canvas_bounding_rect.top);
 	context.arc(e.changedTouches.item(i).pageX -canvas_bounding_rect.left , 
 	e.changedTouches.item(i).pageY - canvas_bounding_rect.top , 10 , 0 , 2 * Math.PI);
 	context.stroke();
