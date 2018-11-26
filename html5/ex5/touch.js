324a293b36a0206555a4b028f763591112807782
minimalkid1 <sabau97@yahoo.com>
2018-11-26 11:28:15 +0200

    touchmove

--- html5/ex5/touch.js
+++ html5/ex5/touch.js
@@ -1,11 +1,24 @@
-document.getElementById("id_business_version").innerHTML="Business version: 2018.11.26.3";
+document.getElementById("id_business_version").innerHTML="Business version: 2018.11.26.4";
 
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
+canvas.addEventListener("touchmove", on_touch_move);
 
 var canvas_bounding_rect = canvas.getBoundingClientRect();
 
 function on_touch_start(e)
+{
+	for(var i=0; i < e.changedTouches.length; i++)
+	{
+	var context = canvas.getContext("2d");
+	context.beginPath();
+	context.arc(e.changedTouches.item(i).pageX -canvas_bounding_rect.left , 
+	e.changedTouches.item(i).pageY - canvas_bounding_rect.top , 10 , 0 , 2 * Math.PI);
+	context.stroke();
+	}
+}
+
+function on_touch_move(e)
 {
 	for(var i=0; i < e.changedTouches.length; i++)
 	{
