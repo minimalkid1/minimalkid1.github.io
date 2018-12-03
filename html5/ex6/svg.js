532af4d2a9a0c9b312354a3f1c0211083e54d7f1
minimalkid1 <sabau97@yahoo.com>
2018-12-03 12:23:33 +0200

    svg

---
+++ html5/ex6/svg.js
@@ -0,0 +1,15 @@
+document.getElementById("id_business_version").innerHTML="Business version: 2018.12.03.0";
+window.addEventListener("deviceorientation",on_device_orientation);
+window.addEventListener("devicemotion",on_device_motion);
+
+function on_device_orientation(e)
+{
+	
+	var svg = document.getElementById ("id_svg");
+	var circle = document.getElementById ("id_circle");
+	
+	var R = 20;
+	
+	circle.setAttribute("cx", svg.width / 2 + e.gamma / 90 * (svg.width / 2 - R));
+	circle.setAttribute("cx", svg.height / 2 + e.beta / 90 * (svg.height / 2 - R));
+}
