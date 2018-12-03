8c1a47b70e597186ddc3d78f26134ae231f935cd
minimalkid1 <sabau97@yahoo.com>
2018-12-03 12:30:33 +0200

    vers

--- html5/ex6/svg.js
+++ html5/ex6/svg.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML="Business version: 2018.12.03.1";
+document.getElementById("id_business_version").innerHTML="Business version: 2018.12.03.2";
 window.addEventListener("deviceorientation",on_device_orientation);
 window.addEventListener("devicemotion",on_device_motion);
 
@@ -10,6 +10,6 @@ function on_device_orientation(e)
 	
 	var R = 20;
 	
-	circle.setAttribute("cx", svg.width / 2 + e.gamma / 90 * (svg.width / 2 - R) + "px");
-	circle.setAttribute("cx", svg.height / 2 + e.beta / 90 * (svg.height / 2 - R) + "px");
+	circle.setAttribute("cx", (svg.width / 2 + e.gamma / 90 * (svg.width / 2 - R)) + "px");
+	circle.setAttribute("cx", (svg.height / 2 + e.beta / 90 * (svg.height / 2 - R)) + "px");
 }
