d952648dc9dc0a5e64a14a192b94862570c01850
minimalkid1 <sabau97@yahoo.com>
2018-12-03 12:34:57 +0200

    try ""

--- html5/ex6/svg.js
+++ html5/ex6/svg.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML="Business version: 2018.12.03.3";
+document.getElementById("id_business_version").innerHTML="Business version: 2018.12.03.4";
 window.addEventListener("deviceorientation",on_device_orientation);
 
 function on_device_orientation(e)
@@ -9,6 +9,6 @@ function on_device_orientation(e)
 	
 	var R = 20;
 	
-	circle.setAttribute("cx", (svg.width / 2 + e.gamma / 90 * (svg.width / 2 - R)) + "px");
-	circle.setAttribute("cx", (svg.height / 2 + e.beta / 90 * (svg.height / 2 - R)) + "px");
+	circle.setAttribute('cx', '(svg.width / 2 + e.gamma / 90 * (svg.width / 2 - R)');
+	circle.setAttribute('cx', '(svg.height / 2 + e.beta / 90 * (svg.height / 2 - R)');
 }
