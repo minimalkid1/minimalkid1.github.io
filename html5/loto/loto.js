9ab792d7428daa4da00f3b305437a719869d7e18
minimalkid1 <sabau97@yahoo.com>
2019-01-18 18:22:19 +0200

    Try Again
    #5

--- html5/loto/loto.js
+++ html5/loto/loto.js
@@ -1,16 +1,16 @@
-document.getElementById("id_business_version").innerHTML="Business version: 2019.01.18.0";
+document.getElementById("id_business_version").innerHTML="Business version: 2019.01.18.1";
 document.getElementById("id_start_button").addEventListener("click", start);
 document.getElementById("id_stop_button").addEventListener("click", stop);
 
-var w1;
+var w;
 
 function start()
 {
-	if(typeof(Worker1) !== "undefined")
+	if(typeof(Worker) !== "undefined")
 	{
-		if(typeof(w1) == "undefined")
+		if(typeof(w) == "undefined")
 		{
-			w = new Worker1("primul_numar.js");
+			w = new Worker("primul_numar.js");
 		}
 		w1.onmessage = function(e)
 		{
@@ -21,6 +21,6 @@ function start()
 
 function stop()
 {
-	w1.terminate();
-	w1 = undefined;
+	w.terminate();
+	w = undefined;
 }
