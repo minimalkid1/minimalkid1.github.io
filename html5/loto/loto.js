4a85b463a878490f0bf6ff21f2a58cbafc298d44
minimalkid1 <sabau97@yahoo.com>
2019-01-18 19:11:41 +0200

    Stop again!
    #19

--- html5/loto/loto.js
+++ html5/loto/loto.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML="Business version: 2019.01.18.9";
+document.getElementById("id_business_version").innerHTML="Business version: 2019.01.18.10";
 document.getElementById("id_start_button").addEventListener("click", start);
 document.getElementById("id_stop_button").addEventListener("click", stop);
 
@@ -71,10 +71,10 @@ function start()
 		{
 			document.getElementById("6Numar").innerHTML = e.data;
 		};
-		
-		sleep(3000);
-		stop();
-	}	
+	}
+
+	sleep(3000);
+	stop();	
 }
 
 function stop()
