ad7d164e5962d300edb3b3c4b45e2ba5770ae112
minimalkid1 <sabau97@yahoo.com>
2019-01-18 18:15:07 +0200

    First Test 1 Number
    #3

---
+++ html5/loto/loto.js
@@ -0,0 +1,26 @@
+document.getElementById("id_business_version").innerHTML="Business version: 2018.10.29.9";
+document.getElementById("id_start_button").addEventListener("click", start);
+document.getElementById("id_stop_button").addEventListener("click", stop);
+
+var w1;
+
+function start()
+{
+	if(typeof(Worker1) !== "undefined")
+	{
+		if(typeof(w1) == "undefined")
+		{
+			w = new Worker1("primul_numar.js");
+		}
+		w1.onmessage = function(e)
+		{
+			document.getElementById("1Numar").innerHTML = e.data;
+		};
+	}
+}
+
+function stop()
+{
+	w1.terminate();
+	w1 = undefined;
+}
