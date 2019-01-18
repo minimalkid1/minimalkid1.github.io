99888fac6c414bc4fe596062ce8156efdc64e9e3
minimalkid1 <sabau97@yahoo.com>
2019-01-18 18:24:14 +0200

    ChangeVer
    #7

--- html5/loto/loto.js
+++ html5/loto/loto.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML="Business version: 2019.01.18.1";
+document.getElementById("id_business_version").innerHTML="Business version: 2019.01.18.2";
 document.getElementById("id_start_button").addEventListener("click", start);
 document.getElementById("id_stop_button").addEventListener("click", stop);
 
